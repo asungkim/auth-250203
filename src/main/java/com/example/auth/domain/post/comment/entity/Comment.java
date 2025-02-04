099e1c7077a8ee9b931879e12aca8811cd5d71d4
asungkim <asungkim007@naver.com>
2025-02-04 09:48:57 +0900

    getItems apply, CommentDto add, protect infinite loop

--- src/main/java/com/example/auth/domain/post/comment/entity/Comment.java
+++ src/main/java/com/example/auth/domain/post/comment/entity/Comment.java
@@ -18,7 +18,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@EntityListeners(AuditingEntityListener.class)
 public class Comment extends BaseTime {
 
     private String content;
